33a8da04689f52ef1c6f7dec185a01ec9dba93b3
Matthew Cohen <mcohen0365@gmail.com>
2019-09-07 02:38:17 -0400

    added file to keep track of globals in program

---
+++ globals.cpp
@@ -0,0 +1,18 @@
+//
+// Created by Matthew Cohen on 2019-09-07.
+//
+
+#include "globals.h"
+
+int SEED = 0;
+int FIN_TIME = 0;
+int ARRIVE_MIN = 0;
+int INIT_TIME = 0;
+int ARRIVE_MAX = 0;
+int QUIT_PROB = 0;
+int CPU_MIN = 0;
+int CPU_MAX = 0;
+int DISK1_MIN = 0;
+int DISK1_MAX = 0;
+int DISK2_MIN = 0;
+int DISK2_MAX = 0;
---
+++ globals.h
@@ -0,0 +1,22 @@
+#ifndef CONST_H
+#define CONST_H
+
+/*
+ * Global constants to be used through out the program
+*/
+
+extern int SEED;
+extern int FIN_TIME;
+extern int ARRIVE_MIN;
+extern int INIT_TIME;
+extern int ARRIVE_MAX;
+extern int QUIT_PROB;
+extern int CPU_MIN;
+extern int CPU_MAX;
+extern int DISK1_MIN;
+extern int DISK1_MAX;
+extern int DISK2_MIN;
+extern int DISK2_MAX;
+
+
+#endif
